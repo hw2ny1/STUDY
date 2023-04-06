74e94bb2bfa4a39b355b99eb9f183d01b06d8600
신성환 <hw2ny1@gmail.com>
2023-04-06 16:03:38 +0900

    '_'

--- c++/STL list.cpp
+++ c++/STL list.cpp
@@ -26,6 +26,23 @@ int main() {
 	list<int> a = {1,2,3,4,5,6,7,8,9,10};
 	for (list<int>::iterator iter = a.begin(); iter != a.end(); iter++)
 		cout << *iter << endl;
+	
+	
+	list<int> temp1;
+	list<int> temp2;
+	for (int i = 0; i < 10; i++) {
+		temp1.push_back(i);
+	}
+
+	for (int i = 11; i < 20; i++) {
+		temp2.push_back(i);
+	}
+	//temp2을 temp1뒤에 붙임
+	temp1.merge(temp2);
+
+	list<int>::iterator iter = temp1.begin();
+	//temp1의 head를 가리키는 포인터를 4만큼 이동
+	advance(iter, 4);
 
 	return 0;
 }
--- 스터디 랜덤.py
+++ 스터디 랜덤.py
@@ -1,6 +1,7 @@
 import random
 study_members = ['민구','창현','정우','정희','성환','세영','혜민','영록']
 bad_guys = []
+problem_num = 5
 try:
     problem_list = []
     while True:
@@ -10,7 +11,6 @@ except EOFError:
         study_members.remove(bad)
     for _ in range(len(bad_guys)):
         study_members.extend(random.sample(study_members, 1))
-    random.shuffle(study_members)
     random.shuffle(problem_list)
-    for i in range(len(study_members)):
-        print(study_members[i],*problem_list[i])
+    for i in range(problem_num):
+        print(study_members[random.randint(0,7)],*problem_list[i])
