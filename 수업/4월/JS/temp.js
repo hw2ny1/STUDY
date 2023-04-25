09fe94848bdb2c560f5820b4f72fbb2e43f9cd4a
hyoonpark <the.day0619@gmail.com>
2023-04-25 17:56:40 +0900

    '_'

---
+++ 수업/4월/JS/temp.js
@@ -0,0 +1,45 @@
+// console.log(a.addEventListener('click', function(){
+//     console.log('ㅠㅠ')
+// }))
+
+// const obj = new Promise((resolve, reject) => {
+//     resolve("성공")
+// })
+
+// obj.then((res) => {
+//     console.log(res)
+// })
+
+// obj.catch((err) => {
+//     console.log(err)
+// })
+
+
+// async function temp(){
+//     ssafy = await molu()
+//     const a = ssafy
+//     console.log(a)
+// }
+
+// temp()
+
+// async function getData(){
+//     const obj = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+//     console.log(obj.data)
+// }
+
+// getData()
+
+async function slowRequest(callBack){
+    console.log('1')
+    await setTimeout( function() {
+        callBack()
+    }, 1000)
+}
+
+function myCallBack(){
+    console.log('2')
+}
+
+slowRequest(myCallBack)
+console.log('3')
--- 스터디 랜덤.py
+++ 스터디 랜덤.py
@@ -1,7 +1,7 @@
 import random
-study_members = [('정우',1),('정희',1),('혜민',1),('성환',1),('세영',1),('민구',1),('영록',1)]
+study_members = [('정희',21),('성환',1),('세영',2),('민구',12),('영록',1)]
 # bad_guys = [('창현',1)]
-problem_num = 5
+problem_num = 6
 study_member = []
 total_n = 0
 
@@ -20,5 +20,6 @@ except EOFError:
     # for _ in range(len(bad_guys)):
     #     study_members.extend(random.sample(study_members, 1))
     random.shuffle(problem_list)
+    cost = [random.randint(1, problem_num * 2) for i in range(problem_num)]
     for i in range(problem_num):
-        print(study_member[random.randint(0,total_n-1)],*problem_list[i])
+        print(study_member[random.randint(0,total_n-1)],*problem_list[i],f'가중치 {cost[i]}')
